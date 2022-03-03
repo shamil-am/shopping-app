21923b982b32f0e3af6531350d4058f8a60f4aa0
shamil-am <shamil.mammadov1@gmail.com>
2022-03-03 09:59:54 +0400

    redux created

---
+++ shopping/src/App.js
@@ -0,0 +1,12 @@
+import { Container } from "reactstrap";
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1>Shopping card</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ shopping/src/redux/reducers/productReducer.js
@@ -0,0 +1,22 @@
+import { ActionTypes } from "../constants/actionTypes";
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "Dibesh",
+      category: "Programming",
+    },
+  ],
+};
+export const productReducer = (
+  state = initialState.products,
+  { type, payload }
+) => {
+  switch (type) {
+    case ActionTypes.SET_PRODUCTS:
+      //   return payload;
+      return state;
+    default:
+      return state;
+  }
+};
